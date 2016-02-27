445a908b9e1194276b1530db3320123783b8ef2c
FilipJoh <filip.johannesson@gmail.com>
2016-02-27 17:51:42 +0100

    fixed createM

--- assignment4/assigment4_scripts/PlotResults_2.m
+++ assignment4/assigment4_scripts/PlotResults_2.m
@@ -19,8 +19,17 @@ v=xB;
 
 
 for i=1:iterations
-   M=createM(v,u)
+   M=createM([v;ones(1,size(v,2))],[u;ones(1,size(u,2))])
    [U,S,V]=svd(M);
-    
-    
+   vprime=V(:,end);
+   H=reshape(vprime(1:9),[3 3])';   
 end
+
+bestH=H;
+
+tform = maketform ('projective', bestH');
+%transfbounds = findbounds ( tform ,[1 1; size (A ,2) size (A ,1)]);
+xdata =[ min([ transfbounds(: ,1); 1]) max([ transfbounds(: ,1); size(B ,2)])];
+ydata =[ min([ transfbounds(: ,2); 1]) max([ transfbounds(: ,2); size(B ,1)])];
+
+[newA] = imtransform(A ,tform , 'xdata', xdata , 'ydata ', ydata );
